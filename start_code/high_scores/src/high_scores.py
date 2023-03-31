df92e7eb091d95da4b31fc35ca506e5677cd3703
Codeclan Student <codeclanstudent@Codeclans-MacBook-Pro.local>
2023-03-31 10:41:40 +0100

    top 3 tie

--- start_code/high_scores/src/high_scores.py
+++ start_code/high_scores/src/high_scores.py
@@ -10,3 +10,8 @@ def personal_best(scores):
 def personal_top_three(scores):
     sorted_scores = sorted(scores, reverse = True)
     return sorted_scores[0:3]
+    
+
+def high_to_low(scores):
+    return sorted(scores, reverse = True)
+
--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import latest, personal_best, personal_top_three, high_to_low
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
@@ -9,11 +9,11 @@ class HighScoresTest(unittest.TestCase):
     
     # Tests
     def setUp(self):
-        self.scores = [2, 4, 10, 2, 5]
+        self.scores = [2, 4, 10, 2, 5, 10]
     # Test latest score (the last thing in the list)
     def test_can_get_latest_score(self):
-        scores = [2, 4, 10, 2, 5]
-        expected_value = 5
+        scores = [2, 4, 10, 2, 5 ,10]
+        expected_value = 10
         actual_value = latest(scores)
         self.assertEqual(expected_value, actual_value)
 
@@ -25,12 +25,20 @@ class HighScoresTest(unittest.TestCase):
 
     # Test top three from list of scores
     def test_can_get_top_three(self):
-        expected_value = [10, 5, 4]
+        expected_value = [10, 10, 5]
         actual_value = personal_top_three(self.scores)
         self.assertEqual(expected_value, actual_value)
     # Test ordered from highest tp lowest
-
+    def test_high_to_low(self):
+        expected_value = [10, 10, 5, 4, 2, 2]
+        actual_value = high_to_low(self.scores)
+        self.assertEqual(expected_value, actual_value)
     # Test top three when there is a tie
+    def test_top_three_tie(self):
+        scores = [ 2, 8, 4, 5, 10, 8, 3]
+        expected_value = [10, 8, 8]
+        actual_value =  personal_top_three(scores)
+        self.assertEqual(expected_value, actual_value)
 
     # Test top three when there are less than three
 
